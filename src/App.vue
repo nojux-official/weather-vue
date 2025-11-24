94df6c11e91b3d6122904201de533ee02758daa9
Nojus Sajauskas <nojus.sajauskas@gmail.com>
2025-11-24 10:34:10 +0200

    Bulma via sass

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,28 @@
 
 <template>
   <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <section class="hero">
+    <div class="hero-body">
+      <p class="title">Hero title</p>
+      <p class="subtitle">Hero subtitle</p>
+    </div>
+  </section>
+  <div class="container">
+  <div class="notification is-primary">
+    This container is <strong>centered</strong> on desktop and larger viewports.
+  </div>
+  <section class="section">
+  <h1 class="title">Section</h1>
+  <h2 class="subtitle">
+    A simple container to d ivide your page into <strong>sections</strong>, like
+    the one you're currently reading.
+  </h2>
+</section>
+</div>
+
+
 </template>
 
-<style scoped></style>
+<style lang="scss">
+@import "bulma";
+</style>
